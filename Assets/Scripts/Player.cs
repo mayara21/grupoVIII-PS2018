5ea6fc6604f7d8beacd191f32f5cc059783ba103
Mayara Martins <mayara.mpf21@gmail.com>
2018-07-06 02:58:17 -0300

    Basic Programming v.3
    Player shooting.
    Enemy behaviour implemented.
    Damage and Death system implemented. Unfinished suction behaviour.
    Need to fix camera, generalize input, and finish implementing suction behaviour.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,11 +7,24 @@ public class Player : MonoBehaviour {
     [SerializeField] float speed = 7f;
     [SerializeField] float jumpSpeed = 7.5f;
     [SerializeField] float controlXSpeedJump = 2 / 3f;
+    [SerializeField] float energyBallSpeed = 8f;
+    [SerializeField] int temporaryDamage = 1;
+    [SerializeField] float knockBackThrustX = 30f;
+    [SerializeField] float knockBackThrustY = 100f;
+    [SerializeField] float slowMoveSpeed = 3f;
+
+    [SerializeField] GameObject energyBallPrefab;
+    [SerializeField] Transform energyBallSpawn;
+    [SerializeField] GameObject suctionArea;
 
     [SerializeField] Cinemachine.CinemachineVirtualCamera cameraDown;
     //[SerializeField] Cinemachine.CinemachineVirtualCamera vcam;
 
-    bool isAlive;
+    int lives = 20;
+    bool isAlive = true;
+    bool canTakeDamage = true;
+    bool canMove = true;
+    bool isSucking = false;
 
     Rigidbody2D rigidBody;
     Collider2D playerCollider2D;
@@ -23,22 +36,57 @@ public class Player : MonoBehaviour {
         rigidBody = GetComponent<Rigidbody2D>();
         playerCollider2D = GetComponent<Collider2D>();
         animator = GetComponent<Animator>();
-
-        controlXSpeedJump *= speed; 
-	}
+  	}
 
     // Update is called once per frame
     void Update() {
-        IsTouchingPlug();
-        Move();
-        Jump();
-        FlipSpriteX();
-
-        if (Input.GetKey(KeyCode.DownArrow) && playerCollider2D.IsTouchingLayers(LayerMask.GetMask("Suspended Platform"))) {
-            playerCollider2D.isTrigger = true;
+        if(!isSucking) {
+            FlipSpriteX();
         }
     }
 
+	private void FixedUpdate() {
+        IsTouchingPlug();
+
+        if(canMove && !isSucking) {
+            Move();
+        }
+
+        if(isSucking) {
+            SlowMove();
+        }
+
+        if(!isSucking) {
+            Jump();
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow) && playerCollider2D.IsTouchingLayers(LayerMask.GetMask("Suspended Platform")))
+        {
+            playerCollider2D.isTrigger = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            Fire();
+        }
+
+        suctionArea.GetComponent<Collider2D>().enabled = false;
+        isSucking = false;
+
+        if (Input.GetKey(KeyCode.X))
+        {
+            SuckEnergyBall();
+        }
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+        if(collision.gameObject.CompareTag("Enemy") && canTakeDamage) {
+            Transform col = collision.gameObject.transform;
+            StartCoroutine(KnockBack(col));
+            StartCoroutine(TakeDamage());
+        }
+	}
 
 	private void LateUpdate() {
         cameraDown.enabled = false;
@@ -54,12 +102,34 @@ public class Player : MonoBehaviour {
         playerCollider2D.isTrigger = false;
 	}
 
-    /*private void OnTriggerStay2D(Collider2D collision) {
-        if (collision.gameObject.tag == "Plug" && Input.GetKeyDown(KeyCode.UpArrow)) {
-            SwitchDimension();
+	private void OnTriggerStay2D(Collider2D collision)
+	{
+        //print(collision.gameObject.tag);
+	}
+
+	private IEnumerator TakeDamage()
+    {
+        lives -= temporaryDamage;
+        if(lives <= 0) {
+            Destroy(gameObject);
         }
-        print(collision.gameObject.tag);
-    }*/
+        canTakeDamage = false;
+        //animator.SetBool("Immune", !canTakeDamage);
+        yield return new WaitForSecondsRealtime(4f);
+
+        canTakeDamage = true;
+        //animator.SetBool("Immune", canTakeDamage);
+
+    }
+
+    private IEnumerator KnockBack(Transform col) {
+        canMove = false;
+        //print(transform.up);
+        rigidBody.AddForce(new Vector2((this.transform.position - col.position).normalized.x * knockBackThrustX, knockBackThrustY));
+        yield return new WaitForSeconds(.5f);
+        canMove = true;
+    }
+
 
 	private void IsTouchingPlug() {
         if (!playerCollider2D.IsTouchingLayers(LayerMask.GetMask("Plug"))) return;
@@ -67,6 +137,41 @@ public class Player : MonoBehaviour {
         if(Input.GetKeyDown(KeyCode.UpArrow)) {
             SwitchDimension();
         }
+    }
+
+    private void SuckEnergyBall() {
+        suctionArea.GetComponent<Collider2D>().enabled = true;
+        isSucking = true;
+    }
+
+    private void SlowMove() {
+        bool isWalking = false;
+        Vector2 playerSpeed = rigidBody.velocity;
+
+        bool isJumping = Mathf.Abs(rigidBody.velocity.y) > 0.0;
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            playerSpeed.x = slowMoveSpeed;
+            isWalking = true;
+        }
+
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            playerSpeed.x = -slowMoveSpeed;
+            isWalking = true;
+        }
+
+        rigidBody.velocity = playerSpeed;
+
+        //animator.SetBool("Walking while Vacuuming", isWalking && isSucking);
+    }
+
+    private void Fire() {
+        var energyBall = (GameObject)Instantiate(energyBallPrefab, energyBallSpawn.position, energyBallSpawn.rotation);
+        energyBall.GetComponent<Rigidbody2D>().velocity = new Vector2(transform.localScale.x * energyBallSpeed, 0f);
+
+        Destroy(energyBall, 3.0f);
     }
 
 	private void Move() {
@@ -77,13 +182,13 @@ public class Player : MonoBehaviour {
 
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            if (isJumping) playerSpeed.x = controlXSpeedJump;
+            if (isJumping) playerSpeed.x = controlXSpeedJump * speed;
             else playerSpeed.x = speed;
             isWalking = true;
         }
 
         else if (Input.GetKey(KeyCode.LeftArrow)) {
-            if (isJumping) playerSpeed.x = -controlXSpeedJump;
+            if (isJumping) playerSpeed.x = -controlXSpeedJump * speed;
             else playerSpeed.x = -speed;
             isWalking = true;
         }
@@ -96,7 +201,7 @@ public class Player : MonoBehaviour {
 
     private void Jump() {
         if (!playerCollider2D.IsTouchingLayers(LayerMask.GetMask("Ground", "Floor Platform", "Suspended Platform")) 
-            || rigidBody.velocity.y != 0.0f) return;
+            || rigidBody.velocity.y != 0f) return;
 
         if(Input.GetKeyDown(KeyCode.Space)) {
             Vector2 jumpSpeedToAdd = new Vector2(0f, jumpSpeed);
@@ -109,12 +214,12 @@ public class Player : MonoBehaviour {
         bool playerHasHorizontalSpeed = Mathf.Abs(rigidBody.velocity.x) > 0.0;
 
         if(playerHasHorizontalSpeed) {
-            transform.localScale = new Vector2(Mathf.Sign(rigidBody.velocity.x), Mathf.Sign(rigidBody.gravityScale));
+            transform.localScale = new Vector2(Mathf.Sign(rigidBody.velocity.x), transform.localScale.y);
         }
     }
 
     private void FlipSpriteY() {
-        transform.localScale = new Vector2(1f, Mathf.Sign(rigidBody.gravityScale));
+        transform.localScale = new Vector2(1f, -1 * transform.localScale.y);
     }
 
     private void SwitchDimension() {
---
+++ Assets/Scripts/SampleEnemy.cs
@@ -0,0 +1,150 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleEnemy : MonoBehaviour {
+
+    [SerializeField] GameObject energyBallPrefab;
+    [SerializeField] Transform energyBallSpawn;
+    //[SerializeField] Player player;
+
+    [SerializeField] float energyBallSpeed = 1f;
+    [SerializeField] int damage = 1;
+    [SerializeField] float speed = 2f;
+    [SerializeField] float postAttackDelay = 2f;
+    [SerializeField] float paralizedTime = 3f;
+
+    [SerializeField] float minimalXPos = 12f;
+    [SerializeField] float maximalXPos = 20f;
+
+    Rigidbody2D rigidBody2D;
+    Collider2D enemyCollider;
+    Animator animator;
+
+    bool canMove = true;
+    bool isAlive = true;
+
+
+	// Use this for initialization
+	void Start () {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+        enemyCollider = GetComponent<Collider2D>();
+        animator = GetComponent<Animator>();
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        HandleMovement();
+
+	}
+
+
+	private void HandleMovement() {
+        if (canMove) {
+            if (IsFacingRight() && this.transform.position.x < maximalXPos) {
+                rigidBody2D.velocity = new Vector2(speed, 0f);
+            }
+
+            else if (!IsFacingRight() && this.transform.position.x > minimalXPos){
+                rigidBody2D.velocity = new Vector2(-speed, 0f);
+            }
+
+            else {
+                SwitchMovement();
+            }
+        }
+
+        else {
+            rigidBody2D.velocity = new Vector2(0f, 0f);
+        }
+
+    }
+
+	private bool IsFacingRight() {
+        return transform.localScale.x > 0;
+    }
+
+    private void SwitchMovement() {
+        transform.localScale = new Vector2(-(Mathf.Sign(rigidBody2D.velocity.x)), 1f);
+    }
+
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+        if(collision.gameObject.CompareTag("Wall")) {
+            SwitchMovement();
+        }
+
+        if(collision.gameObject.CompareTag("My Player")) {
+            StartCoroutine(PostAttackDelay());
+        }
+
+        if(collision.gameObject.CompareTag("Energy Ball")) {
+            StartCoroutine(Paralized());
+        }
+
+        /*if(collision.gameObject.CompareTag("Suction Area")) {
+            Transform col = collision.gameObject.GetComponentInParent<Transform>();
+            SuckEnergyBall(col);
+        }*/
+	}
+
+	/*private void OnCollisionStay2D(Collision2D collision) {
+        if (collision.gameObject.CompareTag("Suction Area"))
+        {
+            Transform col = collision.gameObject.GetComponentInParent<Transform>();
+            SuckEnergyBall(col);
+        }
+	}*/
+
+
+	private IEnumerator PostAttackDelay() {
+        canMove = false;
+        yield return new WaitForSeconds(postAttackDelay);
+        canMove = true;
+    }
+
+    private IEnumerator Paralized() {
+        canMove = false;
+        yield return new WaitForSeconds(paralizedTime);
+        canMove = true;
+    }
+
+    private void Die() {
+        isAlive = false;
+        rigidBody2D.Sleep();
+        //animator.SetBool("Dead", !isAlive);
+    }
+
+    private void SuckEnergyBall(Transform col) {
+        var energyBall = (GameObject)Instantiate(energyBallPrefab, energyBallSpawn.position, energyBallSpawn.rotation);
+        var dir = (col.position - this.transform.position).normalized;
+        energyBall.GetComponent<Rigidbody2D>().velocity = new Vector2(dir.x * energyBallSpeed, 0f);
+    }
+
+
+	/*private void OnTriggerStay2D(Collider2D collision) {
+        print(collision.gameObject.tag);
+        if(enemyCollider.IsTouchingLayers(LayerMask.GetMask("Suction Area"))) {
+        }
+	}*/
+
+	/*private void OnTriggerExit2D(Collider2D collision) // para quando para de tocar o chão (caso inimigo esteja em cima de uma plataforma)
+	{
+        SwitchMovement();
+	}*/
+
+	/*private void OnTriggerEnter2D(Collider2D collision){
+        if(enemyCollider2D.IsTouchingLayers(LayerMask.GetMask("Suction Area"))) {
+            SuctionBall();
+        }
+    }
+
+    private void SuctionBall() {
+        var energyBall = (GameObject)Instantiate(energyBallPrefab, energyBallSpawn.position, energyBallSpawn.rotation);
+        energyBall.GetComponent<Rigidbody2D>().velocity = new Vector2(-1 * player.transform.localScale.x * energyBallSpeed, 0f);
+    }*/
+
+
+}
